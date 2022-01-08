970586ec6899fd0b9f3257f5b1270a627de4243e
Emily <emilyhigginson14@gmail.com>
2022-01-08 14:42:59 -0500

    commit tree acbbb879946eb0abbb88d0d9fccf6a6643f79117

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply)
+
+let num3 = 31;
+let num4 = 9 ;
+let mod = num3 % num4;
+console.log (mod)
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let max = 20
+console.log(Math.max(1, 2, 20));
